dbcb143dc20aa5f1438c7a668125cd9b15c97773
umaira-shaheen <umairaraja01@gmail.com>
2023-04-26 20:41:54 +0500

    CSS Modules

---
+++ src/Content.js
@@ -0,0 +1,14 @@
+import styles from './App.module.css';
+function Content(){
+
+    return(
+        <>
+        <div className={styles.intro}>
+        <h5>Hello! I am Umaira Shaheen. I am doing software Engineering From International Islamic Universty Islamabad</h5>
+
+        </div>
+        </>
+    )
+
+}
+export default Content;
